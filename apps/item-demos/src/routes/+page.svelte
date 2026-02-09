b173076149e65a7ca189b1454426b90420c83947
Eelco Hillenius <eelco.hillenius@gmail.com>
2026-02-09 00:11:42 -0800

    tweak

--- apps/item-demos/src/routes/+page.svelte
+++ apps/item-demos/src/routes/+page.svelte
@@ -60,25 +60,4 @@
 		</a>
 	</div>
 
-	<footer class="alert shadow-lg">
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-			class="stroke-info shrink-0 w-6 h-6"
-		>
-			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-			></path>
-		</svg>
-		<div>
-			<p>
-				All demos use the <strong>IIFE player</strong> loading elements from proxy.pie-api.com.
-				Perfect for learning individual item rendering.
-			</p>
-		</div>
-	</footer>
 </div>
